44d79a02d4f1fe928b051ea68f033577cb76749e
Reinandafaris <reinandafaris7@gmail.com>
2024-04-18 20:26:44 +0700

    Add app.js, errorController.js, package.json, index.js, server.js, and apiError.js files

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const errorController = require("./controllers/errorController");
+const router = require("./routes");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(`${__dirname}/public`));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
+app.use(router);
+
+app.use(errorController.onLost);
+app.use(errorController.onError);
+
+module.exports = app;
